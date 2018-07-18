3be13ce79949749c7aa5d0348200a994a084dd17
qingzhu1224 <yanqingzhu@hualala.com>
2018-07-18 16:00:29 +0800

    xss prevent

--- demo.js
+++ demo.js
@@ -1,3 +1,8 @@
+// & -> &amp;
+// < -> &lt;
+// > -> &gt;
+// " -> &#034;
+// ' -> &#039;
 function preventXSS(str) {
-    return str.replace(/\</g, '&lt').replace(/\>/g, '&gt')
+    return str.replace(/\</g, '&lt').replace(/\>/g, '&gt').replace(/\&/, '&amp');
 }
